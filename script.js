51cef60d8862b2e5b3c2c8ddea1db0f20f87a9c7
RAABIN SINGH  S A <112077212+raabin10@users.noreply.github.com>
2024-08-01 21:00:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstWord(s) {
-  // your code here
+ 
+	 if (s.length === 0) {
+        return '';
+    }
+    
+    // Find the index of the first space
+    let spaceIndex = s.indexOf(' ');
+    
+    // If there is no space, return the whole string
+    if (spaceIndex === -1) {
+        return s;
+    } 
+    
+    // Return the substring from the start to the first space
+    return s.substring(0, spaceIndex);
+	
 }
 
 // Do not change the code below
